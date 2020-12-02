ac90dff09f02f8adb86eb1a352711ad9c38271d4
jihun04 <yujihun20060217@gmail.com>
2020-12-02 15:40:30 +0900

    Update bg.js

--- bg.js
+++ bg.js
@@ -8,28 +8,28 @@ function paintImage(imgNumber) {
     image.src = `images/${imgNumber + 1}.jpg`;
     image.classList.add("bgImage");
     body.prepend(image);
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/1.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/1.jpg") {
         imageName.innerText = `joel-holland-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/2.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/2.jpg") {
         imageName.innerText = `ken-cheung-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/3.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/3.jpg") {
         imageName.innerText = `marcelo-quinan-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/4.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/4.jpg") {
         imageName.innerText = `mohammad-alizade-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/5.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/5.jpg") {
         imageName.innerText = `robert-koorenny-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/6.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/6.jpg") {
         imageName.innerText = `rodion-kutsaev-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/7.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/7.jpg") {
         imageName.innerText = `sergey-pesterev-unsplash`;
     }
-    if(image.src === "file:///C:/Users/yujih/Desktop/momentun/images/8.jpg") {
+    if(image.src === "https://jihun04.github.io/momentun/images/8.jpg") {
         imageName.innerText = `silas-baisch-unsplash`;
     }
 }
