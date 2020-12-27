5c42f098590bed9441ae38e7f41c3ae266a6bb46
jihun04 <yujihun20060217@gmail.com>
2020-12-27 16:28:51 +0900

    Update load-image.js

--- load-image.js
+++ load-image.js
@@ -60,6 +60,7 @@ function handleSelClick(event) {
     const targetImage = target.parentNode.previousSibling;
     const targetImageSrc = targetImage.src;
     bgImage.src = targetImageSrc;
+    localStorage.setItem(CURRENTIMAGESRC_LS, targetImageSrc);
   } else {
     if(target.localName === "div") {
       target.classList.add(CHECKED_CN);
@@ -69,6 +70,7 @@ function handleSelClick(event) {
     const targetImage = target.parentNode.parentNode.previousSibling;
     const targetImageSrc = targetImage.src;
     bgImage.src = targetImageSrc;
+    localStorage.setItem(CURRENTIMAGESRC_LS, targetImageSrc);
   }
 }
 
